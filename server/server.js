d7f53fa40afa5eee237f20b60f517f161dc0c1e4
rdvneltz <ridvanelitez@gmail.com>
2025-11-05 20:05:36 +0300

    fix: CORS ayarı eklendi - frontend URL whitelist

--- server/server.js
+++ server/server.js
@@ -9,7 +9,13 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://fofenk-front.onrender.com'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
